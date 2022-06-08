5cc049ce702f56037b87d23066c263c6350ca938
rowan <rowan.wang00@gmail.com>
2022-06-08 17:02:02 +0100

    develop branch

---
+++ StarlingUI/src/main/java/com/example/starlingui/StarlingUiApplication.java
@@ -0,0 +1,13 @@
+package com.example.starlingui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarlingUiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StarlingUiApplication.class, args);
+    }
+
+}
---
+++ StarlingUI/src/main/java/monitorTab/monitorController.java
@@ -0,0 +1,9 @@
+package monitorTab;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class monitorController {
+
+
+}
