e2bbf7779049321f60dd14fe792b53468c7eb4ba
evansong007 <songevan666@gmai.com>
2022-06-22 00:04:37 +0100

    origins

--- StarlingUI-backend/src/main/java/com/example/starlingui/controller/DesignController.java
+++ StarlingUI-backend/src/main/java/com/example/starlingui/controller/DesignController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.List;
 
-
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/design")
 public class DesignController {
