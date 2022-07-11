9349ff2c36057f294c4b47cc3e89504224403a83
evan <songevan666@gmail.com>
2022-07-11 13:07:08 +0100

    connect websocket

---
+++ StarlingUI-backend/src/main/java/com/example/starlingui/controller/StarlingShellSocketHandler.java
@@ -0,0 +1,9 @@
+package com.example.starlingui.controller;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class StarlingShellSocketHandler extends TextWebSocketHandler {
+
+}
