7cbcd3730f9ac843e16ee412e0c3e22a323533b5
Pench Xie <rm21322@bristol.ac.uk>
2022-07-28 17:05:59 +0100

    add spring default log out setting

--- StarlingUI-backend/src/main/java/com/example/starlingui/config/WebSecurityConfig.java
+++ StarlingUI-backend/src/main/java/com/example/starlingui/config/WebSecurityConfig.java
@@ -14,6 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -39,7 +40,11 @@ public class WebSecurityConfig {
                 .csrf().disable().authorizeHttpRequests()
                 .antMatchers("/design/initialize",  "/login", "/refresh", "/blacklist", "/tokens",
                         "/monitor/*", "/design/database", "/register", "/design/images").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/logout.done").deleteCookies("JSESSIONID")
+                .invalidateHttpSession(true);
         http.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
