55a5f9f2171fb69f40ee9b7ce95935aea80ce174
Oshia Chen <69863250+Oshia-Chen@users.noreply.github.com>
2022-08-02 00:22:20 +0100

    add image tag version to container name

--- StarlingUI-backend/src/main/java/com/example/starlingui/service/TemplatingServiceImp.java
+++ StarlingUI-backend/src/main/java/com/example/starlingui/service/TemplatingServiceImp.java
@@ -150,6 +150,7 @@ public class TemplatingServiceImp implements TemplatingService {
     }
 
 
+
     /**
      * @Description Build the spec of the Container by user's design then append to the pod
      * @param containersArray,pod List of Containers and a PodSpecBuilder
@@ -162,10 +163,7 @@ public class TemplatingServiceImp implements TemplatingService {
             pod.addNewContainer().endContainer();
             Containers ANodeDesign = containersArray.get(i);
             String image = ANodeDesign.getName();
-            String[] imageSplit = image.toLowerCase().split(":");
-            //extract the image name before ":" and replace the possible symbol in image name to "-".
-            //to meet the naming requirement in kubernetes
-            String containerName = imageSplit[0].replaceAll("[_/+.^:,]", "-");
+            String containerName = createContainerName(image);
             String command = ANodeDesign.getCommand();
             String args = ANodeDesign.getArgs();
             List<Env> envArray = ANodeDesign.getEnv();
@@ -186,6 +184,23 @@ public class TemplatingServiceImp implements TemplatingService {
         return pod;
     }
 
+
+    /**
+     * @Description Create a container name by extracting and recombining the image name.
+     * @param image A string of image name
+     * @return String
+     */
+    public String createContainerName(String image){
+        String[] imageSplit = image.toLowerCase().split(":");
+        //extract the image name before ":" and replace the possible symbol in image name to "-".
+        //to meet the naming requirement in kubernetes
+        String containerName = imageSplit[0].replaceAll("[_/+.^:,]", "-");
+        if(imageSplit.length >= 2){
+            containerName += "-" + imageSplit[1].replaceAll("[_/+^:,]", "-");
+        }
+        return containerName;
+    }
+    
 
     /**
      * @Description Build the content of the Env by user's design then append to the ContainerBuilder
