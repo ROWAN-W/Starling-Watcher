ef58cfcc64f4b255b595da026423f6f31a72be7c
evan <songevan666@gmail.com>
2022-08-06 09:09:53 +0100

    error message delete

--- StarlingUI-frontend/src/components/Monitor.js
+++ StarlingUI-frontend/src/components/Monitor.js
@@ -181,7 +181,6 @@ export default function Monitor() {
             return (
                 <>
                     <h1 className='monitor-error'>Fetch failed,Resending request...</h1>
-                    <p className='error-message'>error: {data}</p>
                 </>
             )
         }
